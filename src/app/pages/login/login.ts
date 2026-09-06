78eb0da2dba9f92b0243a3495cdd85471e67ef6c
devbrunosfr <sr.brunosfr@gmail.com>
2026-09-06 17:31:13 -0300

    eu amo paginas de login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  usuario = {
+    nome: '',
+    senha: ''
+  };
+
+  mensagemDeErro: string | null = null;
+
+  constructor(private auth:Auth, private router:Router) {}
+
+  login(){
+
+    this.auth.login(this.usuario).subscribe
+    ({
+      next:(response) => {
+        this.router.navigate(['/home']);
+      },
+      error:(err) => {
+        this.mensagemDeErro = err.error.message || "Usuario ou senha inválidos"
+      }
+    })
+  }
 
 }
---
+++ src/app/services/auth.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+ 
+interface Usuario {
+  nome: string;
+  senha: string;
+}
+ 
+interface LoginResponse {
+  id: number;
+  nome: string;
+  email: string;
+}
+ 
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private apiUrl = 'http://localhost:3001'; // ajuste a porta/URL conforme seu api.js
+ 
+  constructor(private http: HttpClient) {}
+ 
+  login(usuario: Usuario): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, usuario);
+  }
+}
